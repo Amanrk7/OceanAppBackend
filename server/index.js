0b8b0cb03f3ed84d17e55019ee9c72601ef2b250
Aman RK <85789009+Amanrk7@users.noreply.github.com>
2026-04-15 16:57:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -66,6 +66,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(storeMiddleware);
 
 // ═══════════════════════════════════════════════════════════════
 // SERVER TIME
